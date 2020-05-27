c200093da845807feb65bc11333639c6ad4ffe3f
beaming-subroutine-5164 <ben.garlock@gmail.com>
2020-05-27 18:44:29 +0000

    Automatically backed up by Learn

---
+++ lib/square_array.rb
@@ -0,0 +1,12 @@
+def square_array(array)
+  index = 0
+  while index <= array.length do
+    new_array = []
+    for item in array do
+      item = item ** 2
+      new_array.push(item)
+    end
+    index += 1
+    end
+  return new_array
+end
