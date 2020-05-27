91688b326368f25daa5ef5c0e354189f467ac413
beaming-subroutine-5164 <ben.garlock@gmail.com>
2020-05-27 18:33:19 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,6 +2,6 @@ def square_array(array)
   index = 0
   while counter < array.length do
     new_array = []
-    new_array.push(array[index] ** array[index])
+    new_array.push(array[index] ** 2)
     index += 1
 end
